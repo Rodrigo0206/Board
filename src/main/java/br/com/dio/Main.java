e8ed3db669992ba0efe03ab7e52c81477b3eeb29
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2025-05-17 11:29:36 -0300

    criei o main menu

--- src/main/java/br/com/dio/Main.java
+++ src/main/java/br/com/dio/Main.java
@@ -1,7 +1,19 @@
 package br.com.dio;
+import br.com.dio.persistence.migration.MigrationStrategy;
+import br.com.dio.ui.MainMenu;
+
+import java.sql.SQLException;
+
+import static br.com.dio.persistence.config.ConnectionConfig.getConnection;
+
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws SQLException {
+        try(var connection = getConnection()){
+            new MigrationStrategy(connection).executeMigration();
+        }
+        new MainMenu().execute();
     }
+
 }
